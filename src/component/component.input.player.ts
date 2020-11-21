b51b98853f10d541043fe309edd29b5bb89a869b
Olivier Guyot <olivier.guyot@camptocamp.com>
2020-11-21 15:52:22 +0100

    smoother movement, mesh faces its speed vector

--- src/component/component.input.player.ts
+++ src/component/component.input.player.ts
@@ -3,19 +3,43 @@ import BaseInputComponent from './component.input.base';
 import { Vector3 } from '@babylonjs/core';
 
 export default class PlayerInputComponent extends BaseInputComponent {
+  private speedVector = Vector3.Zero();
+
   receiveInput(inputState: GlobalInputState, prevState: GlobalInputState) {
-    const pos = this.transform.getPosition();
+    // speed falloff
+    this.speedVector.scaleInPlace(0.6);
+
+    // speed modification from input
     if (isKeyPressed(inputState, KeyCode.ArrowLeft)) {
-      pos.addInPlace(Vector3.Left().scaleInPlace(0.1));
+      this.speedVector.addInPlace(Vector3.Left().scaleInPlace(0.3));
     }
     if (isKeyPressed(inputState, KeyCode.ArrowUp)) {
-      pos.addInPlace(Vector3.Forward().scaleInPlace(0.1));
+      this.speedVector.addInPlace(Vector3.Forward().scaleInPlace(0.3));
     }
     if (isKeyPressed(inputState, KeyCode.ArrowRight)) {
-      pos.addInPlace(Vector3.Right().scaleInPlace(0.1));
+      this.speedVector.addInPlace(Vector3.Right().scaleInPlace(0.3));
     }
     if (isKeyPressed(inputState, KeyCode.ArrowDown)) {
-      pos.addInPlace(Vector3.Backward().scaleInPlace(0.1));
+      this.speedVector.addInPlace(Vector3.Backward().scaleInPlace(0.3));
     }
+    if (this.speedVector.lengthSquared() > 1) {
+      this.speedVector.normalize();
+    }
+
+    // apply to position
+    this.transform.getPosition().addInPlace(this.speedVector.scale(0.15));
+
+    // set rotation to face speed vector
+    this.transform.setRotation(
+      new Vector3(
+        0,
+        Vector3.GetAngleBetweenVectors(
+          Vector3.Forward(),
+          this.speedVector,
+          Vector3.Up()
+        ),
+        0
+      )
+    );
   }
 }
